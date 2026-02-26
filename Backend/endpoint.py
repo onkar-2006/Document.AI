b584e67f41b855f2415cf14a90d6148656c1f303
onkar kshirsagar <onkarkshirsagar297@gmail.com>
2026-02-26 16:05:47 +0530

    fixing thr bug

--- Backend/endpoint.py
+++ Backend/endpoint.py
@@ -1,6 +1,6 @@
 import os, uuid, shutil
 from fastapi import FastAPI, UploadFile, File, BackgroundTasks, HTTPException,Form
-from typing import Optional
+from typing_extensions import Optional
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 from langchain_core.messages import HumanMessage
